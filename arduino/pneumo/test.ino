48c962ace326d11e622fe33ecab6c1b017afe2ea
kisate <dhkas@mail.ru>
2019-04-22 23:23:26 +0300

    pneumo p.1

--- arduino/pneumo/test.ino
+++ arduino/pneumo/test.ino
@@ -1,6 +1,17 @@
-#define UPBORDER = 7
-#define DOWNBORDER = 9 
+#define UPBORDER 9
+#define DOWNBORDER 7 
+#define ARMSMOTOR 
 void setup()
 {
-  pinMode(UPBORDER, INPUT_PULLUP);
+  pinMode(DOWNBORDER, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(DOWNBORDER) == 1)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else digitalWrite(13, LOW);
 }
